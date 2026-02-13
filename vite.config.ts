105d1e0c8b289d8be32a34704720ec470f2d81e8
Calokaa <zserick999@gmail.com>
2026-02-13 19:44:57 +0200

    Update vite.config.ts
    Fix vite.config.ts syntax for GitHub Pages
    
    Signed-off-by: Calokaa <zserick999@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/share-sell-connect/', // 👈 ureke double quotes niba phone yawe ibyanga
+  base: '/share-sell-connect/', // 
   plugins: [react()]
 })
