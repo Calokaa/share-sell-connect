ec19e49a59180cceeebcb726ad85259eeca5f6c2
Calokaa <zserick999@gmail.com>
2026-02-13 19:35:57 +0200

    Update vite.config.ts
    Fix vite.config.ts syntax for GitHub Pages
    
    Signed-off-by: Calokaa <zserick999@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/share-sell-connect/",   
-  plugins: [react()],
+  base: '/share-sell-connect/', // 👈 ureke double quotes niba phone yawe ibyanga
+  plugins: [react()]
 })
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
